38a147dae5e426dd6fb2c0b69a3d24da2e7ac323
Jang Junhyuck <ggogono2@gmail.com>
2021-08-30 17:51:40 +0900

    4 username finish

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 const greeting = document.getElementById("greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+
 
 function onLoginSubmit(event) {
     event.preventDefault();
@@ -10,7 +12,18 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.textContent = `Hello! ${username}`;
+    localStorage.setItem(USERNAME_KEY,username);
 
 }
 
-loginForm.addEventListener("submit",onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+
+} else{
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.textContent = `Hello! ${savedUsername}`;
+}
