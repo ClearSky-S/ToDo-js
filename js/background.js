5c15f45d002a882847858e563ce8c77d4f779011
Jang Junhyuck <ggogono2@gmail.com>
2021-09-05 19:07:24 +0900

    work to do

--- js/background.js
+++ js/background.js
@@ -1,6 +1,9 @@
+// works todo: artist name
+
 const images = ["1.jpg","2.png","3.jpg","5.jpg","6.jpg","7.png","8.jpg"];
 let index = Math.floor(Math.random()*images.length);
 // index = 6; //temp 
+console.log("background index: "+index);
 const chooseImage = images[index];
 const bgImage = document.createElement("img");
 const backgroundImageContainer = document.querySelector("#background-image-container");
